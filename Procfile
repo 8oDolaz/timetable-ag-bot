519e17734206d78c956ca57adffcc5441a44b844
glebari <gzagranichnov@mail.ru>
2020-10-19 11:07:12 +0400

    chaged files

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-clock: python clock.py $PORT worker: python app.py $PORT + 1
+worker: python app.py $PORT
--- app.py
+++ app.py
@@ -1,13 +1,61 @@
 import telebot
 import json
+import datetime
+import requests
+from bs4 import BeautifulSoup
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 bot = telebot.TeleBot('1382842329:AAGm6ydcY0mybVfkLxwH7q0rAkqF9S7hh8M')  # bot with our token
+sched = BlockingScheduler()
 
 keyboard1 = telebot.types.ReplyKeyboardMarkup()  # add a keyboard
 button1 = telebot.types.KeyboardButton('Сегодня')
 button2 = telebot.types.KeyboardButton('Завтра')
 button3 = telebot.types.KeyboardButton('На неделю')
 keyboard1.row(button1, button2, button3)  # add it all to one row
+
+
+@sched.scheduled_job('interval', minutes=1)
+def parse_timetable():
+    now = datetime.datetime.now()  # today's date
+    now = now.strftime('%Y-%m-%d')
+
+    url_lessons = f'https://timetable.spbu.ru/AGSM/StudentGroupEvents/Primary/275100/%s' % now  # timetable site
+    r = requests.get(url_lessons, headers={'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'})
+    # send a get request
+
+    soup = BeautifulSoup(r.text, features='lxml')  # init Beautiful soup
+    all_info = soup.find_all('div', {'class': 'panel panel-default'})[2:]
+    # all info except first to (we don't need that)
+
+    all_data = []  # empty array for all information
+
+    with open('data_base.json', 'w') as file:  # clear our database
+        file.write('')
+        file.close()
+
+    for item in all_info:  # go in all info
+        title_m, time_m = [], []  # empty arrays for time and subject
+        date = item.find('h4', {'class': 'panel-title'}).text.replace(' ', '', 20).replace('\n', '').replace('\r', '')
+        date = date[::-1].replace(' ', '', 16)[::-1]  # today's date
+
+        for i in item.find_all('span', {'class': 'moreinfo', 'title': 'Время'}):
+            time = i.text.replace(' ', '').replace('\n', '').replace('\r', '')
+            time_m.append(time)  # find and add all lessons timing
+
+        for i in item.find_all('span', {'class': 'moreinfo', 'title': 'Предмет'}):
+            title = i.text.replace(' ', '', 16).replace('\n', '').replace('\r', '')
+            title = title[:(title.find(','))]
+            title_m.append(title)  # find and add all lessons title
+
+        all_data.append([{date: [time_m, title_m]}])  # add everything to the array
+
+    with open('data_base.json', 'a') as file:  # add the array to a database
+        file.write(json.dumps(all_data))
+        file.close()
+
+    print(1)
+
 
 
 def prepare_answer(data, s=''):
@@ -70,3 +118,4 @@ def main_bot(message):
 
 
 bot.polling()  # start infinity circle
+sched.start()
