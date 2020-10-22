ee63d436f1ee17bea2a819055218c45726e5e833
glebari <gzagranichnov@mail.ru>
2020-10-22 11:07:25 +0400

    databse update

--- app.py
+++ app.py
@@ -3,123 +3,126 @@ import json
 import datetime
 import requests
 from bs4 import BeautifulSoup
-# import asyncio
-# from apscheduler.schedulers.blocking import BlockingScheduler
-
-bot = telebot.TeleBot('1382842329:AAGm6ydcY0mybVfkLxwH7q0rAkqF9S7hh8M')  # bot with our token
+import threading
+import time as tm
 
 
-# sched = BlockingScheduler()
-
-
-# @sched.scheduled_job('interval', minutes=1)
 def parse_timetable():
-    now = datetime.datetime.now()  # today's date
-    now = now.strftime('%Y-%m-%d')
+    while True:
+        tm.sleep(3)
+        now = datetime.datetime.now()  # today's date
+        now = now.strftime('%Y-%m-%d')
 
-    url_lessons = f'https://timetable.spbu.ru/AGSM/StudentGroupEvents/Primary/275100/%s' % now  # timetable site
-    r = requests.get(url_lessons, headers={'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'})
-    # send a get request
+        url_lessons = f'https://timetable.spbu.ru/AGSM/StudentGroupEvents/Primary/275100/%s' % now  # timetable site
+        r = requests.get(url_lessons, headers={'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'})
+        # send a get request
 
-    soup = BeautifulSoup(r.text, features='lxml')  # init Beautiful soup
-    all_info = soup.find_all('div', {'class': 'panel panel-default'})[2:]
-    # all info except first to (we don't need that)
+        soup = BeautifulSoup(r.text, features='lxml')  # init Beautiful soup
+        all_info = soup.find_all('div', {'class': 'panel panel-default'})[2:]
+        # all info except first to (we don't need that)
 
-    all_data = []  # empty array for all information
+        all_data = []  # empty array for all information
 
-    with open('data_base.json', 'w') as file:  # clear our database
-        file.write('')
-        file.close()
-
-    for item in all_info:  # go in all info
-        title_m, time_m = [], []  # empty arrays for time and subject
-        date = item.find('h4', {'class': 'panel-title'}).text.replace(' ', '', 20).replace('\n', '').replace('\r', '')
-        date = date[::-1].replace(' ', '', 16)[::-1]  # today's date
-
-        for i in item.find_all('span', {'class': 'moreinfo', 'title': 'Время'}):
-            time = i.text.replace(' ', '').replace('\n', '').replace('\r', '')
-            time_m.append(time)  # find and add all lessons timing
-
-        for i in item.find_all('span', {'class': 'moreinfo', 'title': 'Предмет'}):
-            title = i.text.replace(' ', '', 16).replace('\n', '').replace('\r', '')
-            title = title[:(title.find(','))]
-            title_m.append(title)  # find and add all lessons title
-
-        all_data.append([{date: [time_m, title_m]}])  # add everything to the array
-
-    with open('data_base.json', 'a') as file:  # add the array to a database
-        file.write(json.dumps(all_data))
-        file.close()
-
-    print(1)
-
-
-keyboard1 = telebot.types.ReplyKeyboardMarkup()  # add a keyboard
-button1 = telebot.types.KeyboardButton('Сегодня')
-button2 = telebot.types.KeyboardButton('Завтра')
-button3 = telebot.types.KeyboardButton('На неделю')
-keyboard1.row(button1, button2, button3)  # add it all to one row
-
-
-def prepare_answer(data, s=''):
-    for item in data[0].keys():  # get all information that we need
-        s += item + '\n' + '\n'  # do nice view
-        for i in range(len(data[0].get(item)[0])):  # print all day info
-            s += data[0].get(item)[0][i] + ' ' + data[0].get(item)[1][i] + '\n'  # first is time, second is subject
-    return s
-
-
-@bot.message_handler(func=lambda message: True, commands=['start'])  # just to start use the bot
-def start(message):
-    bot.send_message(message.chat.id, 'Добрый день!', reply_markup=keyboard1)
-
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])  # to see a timetable
-def main_bot(message):
-    if message.text.lower() == 'сегодня':  # to see today's timetable
-
-        with open('data_base.json', 'r') as file:  # get everything we need from db
-            data = json.loads(file.read())
-            file.close()
-            data = data[0]
-
-        answer = prepare_answer(data)
-
-        bot.send_message(message.chat.id, answer, reply_markup=keyboard1)  # send a message with timetable
-
-    elif message.text.lower() == 'завтра':  # to see tomorrow's timetable
-
-        with open('data_base.json', 'r') as file:  # get everything we need from db
-            data = json.loads(file.read())
-            file.close()
-            data = data[1]
-
-        answer = prepare_answer(data)
-
-        bot.send_message(message.chat.id, answer, reply_markup=keyboard1)  # send a message with timetable
-
-    elif message.text.lower() == 'на неделю':  # to see week's timetable
-
-        with open('data_base.json', 'r') as file:  # get all information from db expect today's info
-            data = json.loads(file.read())
-            data = data[1:]
+        with open('data_base.json', 'w') as file:  # clear our database
+            file.write('')
             file.close()
 
-        answer = ''
-        for item in data:  # add all timetable to a string
-            answer += '\n' + '\n'
-            for v in item[0].keys():
-                answer += v + '\n'
-                for i in range(len(item[0].get(v)[0])):
-                    answer += item[0].get(v)[0][i] + ' ' + item[0].get(v)[1][i] + '\n'
+        for item in all_info:  # go in all info
+            title_m, time_m = [], []  # empty arrays for time and subject
+            date = item.find('h4', {'class': 'panel-title'}).text.replace(' ', '', 20).replace('\n', '').replace('\r', '')
+            date = date[::-1].replace(' ', '', 16)[::-1]  # today's date
 
-        bot.send_message(message.chat.id, answer, reply_markup=keyboard1)  # send a message
+            for i in item.find_all('span', {'class': 'moreinfo', 'title': 'Время'}):
+                time = i.text.replace(' ', '').replace('\n', '').replace('\r', '')
+                time_m.append(time)  # find and add all lessons timing
 
-    else:
+            for i in item.find_all('span', {'class': 'moreinfo', 'title': 'Предмет'}):
+                title = i.text.replace(' ', '', 16).replace('\n', '').replace('\r', '')
+                title = title[:(title.find(','))]
+                title_m.append(title)  # find and add all lessons title
 
-        bot.send_message(message.chat.id, 'Выберете одну из опций', reply_markup=keyboard1)
+            all_data.append([{date: [time_m, title_m]}])  # add everything to the array
+
+        with open('data_base.json', 'a') as file:  # add the array to a database
+            file.write(json.dumps(all_data))
+            file.close()
+
+        print(1)
+print(parse_timetable())
 
 
-bot.polling(none_stop=True)
+def main():
+    bot = telebot.TeleBot('1382842329:AAGm6ydcY0mybVfkLxwH7q0rAkqF9S7hh8M')  # bot with our token
 
-# sched.start()
+    keyboard1 = telebot.types.ReplyKeyboardMarkup()  # add a keyboard
+    button1 = telebot.types.KeyboardButton('Сегодня')
+    button2 = telebot.types.KeyboardButton('Завтра')
+    button3 = telebot.types.KeyboardButton('На неделю')
+    keyboard1.row(button1, button2, button3)  # add it all to one row
+
+    def prepare_answer(data, s=''):
+        for item in data[0].keys():  # get all information that we need
+            s += item + '\n' + '\n'  # do nice view
+            for i in range(len(data[0].get(item)[0])):  # print all day info
+                s += data[0].get(item)[0][i] + ' ' + data[0].get(item)[1][i] + '\n'  # first is time, second is subject
+        return s
+
+    @bot.message_handler(func=lambda message: True, commands=['start'])  # just to start use the bot
+    def start(message):
+        bot.send_message(message.chat.id, 'Добрый день!', reply_markup=keyboard1)
+
+    @bot.message_handler(func=lambda message: True, content_types=['text'])  # to see a timetable
+    def main_bot(message):
+        if message.text.lower() == 'сегодня':  # to see today's timetable
+
+            with open('data_base.json', 'r') as file:  # get everything we need from db
+                data = json.loads(file.read())
+                file.close()
+                data = data[0]
+
+            answer = prepare_answer(data)
+
+            bot.send_message(message.chat.id, answer, reply_markup=keyboard1)  # send a message with timetable
+
+        elif message.text.lower() == 'завтра':  # to see tomorrow's timetable
+
+            with open('data_base.json', 'r') as file:  # get everything we need from db
+                data = json.loads(file.read())
+                file.close()
+                data = data[1]
+
+            answer = prepare_answer(data)
+
+            bot.send_message(message.chat.id, answer, reply_markup=keyboard1)  # send a message with timetable
+
+        elif message.text.lower() == 'на неделю':  # to see week's timetable
+
+            with open('data_base.json', 'r') as file:  # get all information from db expect today's info
+                data = json.loads(file.read())
+                data = data[1:]
+                file.close()
+
+            answer = ''
+            for item in data:  # add all timetable to a string
+                answer += '\n' + '\n'
+                for v in item[0].keys():
+                    answer += v + '\n'
+                    for i in range(len(item[0].get(v)[0])):
+                        answer += item[0].get(v)[0][i] + ' ' + item[0].get(v)[1][i] + '\n'
+
+            bot.send_message(message.chat.id, answer, reply_markup=keyboard1)  # send a message
+
+        else:
+
+            bot.send_message(message.chat.id, 'Выберете одну из опций', reply_markup=keyboard1)
+
+    bot.polling(none_stop=True)
+
+
+if __name__ == '__main__':
+    main_thread = threading.Thread(target=main(), name='testing my threads', daemon=True)
+    parser_thread = threading.Thread(target=parse_timetable(), name='parser_thread', daemon=True)
+    parser_thread.start()
+    main_thread.start()
+    parser_thread.join()
+    main_thread.join()
