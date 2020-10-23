7f64692e028995074997233f18e4098804b72a6a
glebari <gzagranichnov@mail.ru>
2020-10-23 22:41:47 +0400

    switch from json to postgresql (to prevent update problem)

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import telebot
-import json
+import psycopg2 as ps2
 
 
 def main():
+    connection = ps2.connect(
+        database='dfsp85rbt6tna2',
+        host='ec2-54-217-204-34.eu-west-1.compute.amazonaws.com',
+        user='rmpqgvxcfahbdg',
+        password='935a6af6648144a4044c436a75d94d989084cb84d19c8229a6b6c9690240aafb',
+        port='5432',
+    )
+
+    cursor = connection.cursor()
+
     bot = telebot.TeleBot('1382842329:AAGm6ydcY0mybVfkLxwH7q0rAkqF9S7hh8M')  # bot with our token
 
     keyboard1 = telebot.types.ReplyKeyboardMarkup()  # add a keyboard
@@ -11,12 +21,22 @@ def main():
     button3 = telebot.types.KeyboardButton('На неделю')
     keyboard1.row(button1, button2, button3)  # add it all to one row
 
-    def prepare_answer(data, s=''):
-        for item in data[0].keys():  # get all information that we need
-            s += item + '\n' + '\n'  # do nice view
-            for i in range(len(data[0].get(item)[0])):  # print all day info
-                s += data[0].get(item)[0][i] + ' ' + data[0].get(item)[1][i] + '\n'  # first is time, second is subject
-            return s
+    def get_time_title(day):
+        cursor.execute('SELECT * FROM LESSONS_TIME WHERE DAY_NAME=%s', (day,))
+        time = [item[0] for item in cursor.fetchall()]  # item[0] because second field is day and we don't need it
+
+        cursor.execute('SELECT * FROM LESSONS_TITLE WHERE DAY_NAME=%s', (day,))
+        title = [item[0] for item in cursor.fetchall()]  # item[0] because second field is day and we don't need it
+
+        return time, title
+
+    def prepare_answer(day, time, title, s=''):
+        # day[0].upper() - first letter is now upper!
+        s += day[0].upper() + day[1:] + '\n'
+        for item in range(len(time)):
+            # I don't know where is the problem but I need to delete spaces here
+            s += time[item].replace(' ', '') + ' ' + title[item] + '\n'
+        return s
 
     @bot.message_handler(func=lambda message: True, commands=['start'])  # just to start use the bot
     def start(message):
@@ -26,40 +46,34 @@ def main():
     def main_bot(message):
         if message.text.lower() == 'сегодня':  # to see today's timetable
 
-            with open('data_base.json', 'r') as file:  # get everything we need from db
-                data = json.loads(file.read())
-                file.close()
-                data = data[0]
+            cursor.execute('SELECT * FROM DAY')
+            day = cursor.fetchall()[0][0]
+            time, title = get_time_title(day)
 
-            answer = prepare_answer(data)
+            answer = prepare_answer(day, time, title)
 
             bot.send_message(message.chat.id, answer, reply_markup=keyboard1)  # send a message with timetable
 
         elif message.text.lower() == 'завтра':  # to see tomorrow's timetable
 
-            with open('data_base.json', 'r') as file:  # get everything we need from db
-                data = json.loads(file.read())
-                file.close()
-                data = data[1]
+            cursor.execute('SELECT * FROM DAY')
+            day = cursor.fetchall()[1][0]
+            time, title = get_time_title(day)
 
-            answer = prepare_answer(data)
+            answer = prepare_answer(day, time, title)
 
             bot.send_message(message.chat.id, answer, reply_markup=keyboard1)  # send a message with timetable
 
         elif message.text.lower() == 'на неделю':  # to see week's timetable
 
-            with open('data_base.json', 'r') as file:  # get all information from db expect today's info
-                data = json.loads(file.read())
-                data = data[1:]
-                file.close()
-
             answer = ''
-            for item in data:  # add all timetable to a string
-                answer += '\n' + '\n'
-                for v in item[0].keys():
-                    answer += v + '\n'
-                    for i in range(len(item[0].get(v)[0])):
-                        answer += item[0].get(v)[0][i] + ' ' + item[0].get(v)[1][i] + '\n'
+
+            cursor.execute('SELECT * FROM DAY')
+            days = cursor.fetchall()
+            for day in days:
+                # we need to take first one to prevent error (there are only one object)
+                time, title = get_time_title(day[0])
+                answer += prepare_answer(day[0], time, title) + '\n'
 
             bot.send_message(message.chat.id, answer, reply_markup=keyboard1)  # send a message
 
--- parser.py
+++ parser.py
@@ -1,4 +1,3 @@
-import json
 import datetime
 import requests
 from bs4 import BeautifulSoup
@@ -6,25 +5,24 @@ from bs4 import BeautifulSoup
 
 def parse_timetable():
     now = datetime.datetime.now()  # today's date
-    now = now.strftime('%Y-%m-%d')
+    now = now.strftime('%Y-%m-%d')  # example: 2020-09-09
 
-    url_lessons = f'https://timetable.spbu.ru/AGSM/StudentGroupEvents/Primary/275100/%s' % now  # timetable site
-    r = requests.get(url_lessons, headers={'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'})
+    # website link for actual day
+    url_lessons = f'https://timetable.spbu.ru/AGSM/StudentGroupEvents/Primary/275100/%s' % now
     # send a get request
+    request = requests.get(url_lessons, headers={'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'})
 
-    soup = BeautifulSoup(r.text, features='lxml')  # init Beautiful soup
+    soup = BeautifulSoup(request.text, features='lxml')  # init Beautiful soup
+    # all info except first and second (some useless info in it)
     all_info = soup.find_all('div', {'class': 'panel panel-default'})[2:]
-    # all info except first to (we don't need that)
 
     all_data = []  # empty array for all information
 
-    with open('data_base.json', 'w') as file:  # clear our database
-        file.write('')
-        file.close()
-
     for item in all_info:  # go in all info
         title_m, time_m = [], []  # empty arrays for time and subject
+        # prepare data (a lot of useless symbols)
         date = item.find('h4', {'class': 'panel-title'}).text.replace(' ', '', 20).replace('\n', '').replace('\r', '')
+        # replacing spaces after text
         date = date[::-1].replace(' ', '', 16)[::-1]  # today's date
 
         for i in item.find_all('span', {'class': 'moreinfo', 'title': 'Время'}):
@@ -32,15 +30,10 @@ def parse_timetable():
             time_m.append(time)  # find and add all lessons timing
 
         for i in item.find_all('span', {'class': 'moreinfo', 'title': 'Предмет'}):
-            title = i.text.replace(' ', '', 16).replace('\n', '').replace('\r', '')
+            title = i.text.replace(' ', '', 15).replace('\n', '').replace('\r', '')
             title = title[:(title.find(','))]
             title_m.append(title)  # find and add all lessons title
 
-        all_data.append([{date: [time_m, title_m]}])  # add everything to the array
+        all_data.append({date: [time_m, title_m]})  # add everything to the array
 
-    with open('data_base.json', 'a') as file:  # add the array to a database
-        file.write(json.dumps(all_data))
-        file.close()
-
-
-parse_timetable()
+    return all_data
