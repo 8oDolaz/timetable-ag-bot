440a9625ac50efa529404acccb7da7e34ee84f8c
8oDolaz <g.zagranichnov@gmail.com>
2020-10-28 23:23:50 +0400

    optimization update

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import datetime
 import requests
 from bs4 import BeautifulSoup
+import psycopg2 as ps2
 
 
 def parse_timetable():
@@ -37,3 +38,33 @@ def parse_timetable():
         all_data.append({date: [time_m, title_m]})  # add everything to the array
 
     return all_data
+
+
+def database_update(data):
+    connection = ps2.connect(
+        database='dfsp85rbt6tna2',
+        host='ec2-54-217-204-34.eu-west-1.compute.amazonaws.com',
+        user='rmpqgvxcfahbdg',
+        password='935a6af6648144a4044c436a75d94d989084cb84d19c8229a6b6c9690240aafb',
+        port='5432',
+    )
+
+    cursor = connection.cursor()
+    cursor.execute('TRUNCATE DAY CASCADE;')
+
+    for iteration in range(len(data)):  # all info in array so we need to go throw it
+        for day in data[iteration].keys():  # goes throw all objects
+            cursor.execute('INSERT INTO DAY(DAY_NAME) VALUES (%s);', (day,))
+
+            for time in data[iteration].get(day)[0]:  # [0] after get means that we take all subjects time
+                cursor.execute('INSERT INTO LESSONS_TIME(TIME, DAY_NAME) VALUES (%s, %s);', (time, day,))
+
+            for title in data[iteration].get(day)[1]:  # [1] after get means that we take all subjects title
+                cursor.execute('INSERT INTO LESSONS_TITLE(TITLE, DAY_NAME) VALUES (%s, %s);', (title, day,))
+
+    connection.commit()
+    cursor.close()
+    connection.close()  # determinate connection
+
+
+database_update(parse_timetable())  # run parsing and write all date to db
